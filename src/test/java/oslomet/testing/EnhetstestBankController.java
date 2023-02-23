32da7f53e3b7440e8b40c01f259fce7eb80b3ecc
Ylligg <yll.gashi@hotmail.com>
2023-02-23 12:25:52 +0100

    klarte lagre og hente kunde tester

--- src/test/java/oslomet/testing/EnhetstestBankController.java
+++ src/test/java/oslomet/testing/EnhetstestBankController.java
@@ -1,5 +1,6 @@
 package oslomet.testing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -120,13 +121,43 @@ public class EnhetstestBankController {
     @Test
     public void test_lagreKunde() {
 
-        when(sjekk.loggetInn()).thenReturn(null);
+        Kunde kunde1 = new Kunde("12345654321", "Ylli", "Gashi", "Olebrummsvei 27", "1054", "OSLO", "40168017","ylli1105");
+
+        when(sjekk.loggetInn()).thenReturn("12345654321");
+
+        Mockito.when(adminKundeRepository.registrerKunde((any(Kunde.class)))).thenReturn("OK");
 
         // act
-        List<Konto> resultat = bankController.hentKonti();
+        String resultat = adminKundeController.lagreKunde(kunde1);
 
         // assert
-        assertNull(resultat);
+        assertEquals("OK",resultat);
     }
+
+    @Test
+    public void test_HentAlleOK() {
+
+        // lager kunder og liste
+
+        Kunde kunde1 = new Kunde("12345678911", "Ylli", "Gashi", "Olebrummsvei 27", "1054", "OSLO", "40168017","ylli1105");
+        Kunde kunde2 = new Kunde("67676767676", "Ulaksh", "Gashi", "Olebrummsvei 27", "1054", "OSLO", "40168017","ylli1105");
+        List<Kunde> kundeliste = new ArrayList<>();
+        kundeliste.add(kunde1);
+        kundeliste.add(kunde2);
+
+        // ser om at de er logget inn
+
+        when(sjekk.loggetInn()).thenReturn("12345678911");
+
+        // kaller på medtoden og lister ut kundene
+
+        Mockito.when(adminKundeRepository.hentAlleKunder()).thenReturn(kundeliste);
+
+        List<Kunde> resultat = adminKundeController.hentAlle();
+
+        Assert.assertEquals(kundeliste, resultat);
+
+    }
+
 }
 
